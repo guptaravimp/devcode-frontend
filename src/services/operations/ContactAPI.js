17bb4f34af6f697a00e2f729c95d2b8c31f52be1
Ravi Gupta <142169363+guptaravimp@users.noreply.github.com>
2025-04-23 11:56:26 +0530

    Update ContactAPI.js

--- src/services/operations/ContactAPI.js
+++ src/services/operations/ContactAPI.js
@@ -1,37 +1,4 @@
-// import toast from "react-hot-toast"
-// import { apiconnector } from "../apiconnector"
-// import { contactusEndpoint, endpoints } from "../apis"
-// import { setLoading, setToken } from "../../slices/authSlice"
 
-// const {
-//     CONTACT_US_API,
-// } = contactusEndpoint;
-
-
-
-// export function submitContactUs(data) {
-//     // const navigate=useNavigate()
-//     return async (dispatch) => {
-//         dispatch(setLoading(true));
-//         try {
-//             const response = await apiconnector("POST", CONTACT_US_API, {
-//                 data,
-//             })
-//             // const response={status:"OK"}
-//             toast.success("Message sent successfully!");
-//             dispatch(setToken(response.data.token))
-//             console.log("CONTACT US RESPONSE:", response);
-//             // navigate("/");
-//         } catch (error) {
-//             console.log("Error occurred:", error);
-//             toast.error("Failed to send message.");
-//         } finally {
-//             dispatch(setLoading(false));
-//         }
-//     };
-
-
-// }
 
 import toast from "react-hot-toast"
 // import { apiconnector } from "../apiconnector"
@@ -40,7 +7,7 @@ import { contactusEndpoint } from "../apis"
 // setLoading
 import { useSelector } from "react-redux";
 import { setLoading } from "../../slices/authSlice";
-import { apiconnector } from "../apiConnector";
+import { apiconnector } from "../apiconnector";
 
 const {
     CONTACT_US_API,
