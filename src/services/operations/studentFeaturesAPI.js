3b83b6f7bc9c37def380fcd61bfe89b74fedc9c8
Ravi Gupta <142169363+guptaravimp@users.noreply.github.com>
2025-04-23 11:57:01 +0530

    Update studentFeaturesAPI.js

--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -1,5 +1,5 @@
 import {toast} from "react-hot-toast";
-import { apiconnector } from "../apiConnector";
+import { apiconnector } from "../apiconnector";
 import rzpLogo from "../../assets/Logo/rzp_logo.png"
 import { resetCart } from "../../slices/cartSlice";
 import { studentEndpoints } from "../apis";
